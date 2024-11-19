23438a207f5e85676fe27196157aae4d26fc4004
crocodile13 <134718087+crocodile13@users.noreply.github.com>
2024-11-19 20:57:40 +0100

    Update detect.py

--- detect.py
+++ detect.py
@@ -46,8 +46,8 @@ genderProto="gender_deploy.prototxt"
 genderModel="gender_net.caffemodel"
 
 MODEL_MEAN_VALUES=(78.4263377603, 87.7689143744, 114.895847746)
-ageList=['(0-2)', '(4-6)', '(8-12)', '(15-20)', '(25-32)', '(38-43)', '(48-53)', '(60-100)']
-genderList=['Male','Female']
+ageList=['0-2', '4-6', '8-12', '15-20', '25-32', '38-43', '48-53', '60-100']
+genderList=['male','female']
 
 faceNet=cv2.dnn.readNet(faceModel,faceProto)
 ageNet=cv2.dnn.readNet(ageModel,ageProto)
@@ -90,13 +90,15 @@ else:
     genderNet.setInput(blob)
     genderPreds = genderNet.forward()
     gender = genderList[genderPreds[0].argmax()]
-    print(f'Gender: {gender}')
+#    print(f'Gender: {gender}')
 
     # Prédiction de l'âge
     ageNet.setInput(blob)
     agePreds = ageNet.forward()
     age = ageList[agePreds[0].argmax()]
-    print(f'Age: {age[1:-1]} years')
+#    print(f'Age: {age[1:-1]} years')
+    print(f'{gender}_{age}')
+
 
     # Ajout des informations sur l'image
     cv2.putText(resultImg, f'{gender}, {age}', (faceBox[0], faceBox[1] - 10),
